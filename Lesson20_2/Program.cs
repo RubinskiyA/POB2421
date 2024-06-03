790ad8e00d167d9c91a833a8d82bc9e5eb01b265
SuperPC <SuperPC@SUPERPC>
2024-06-03 14:53:19 +0200

    hw

---
+++ Lesson20_2/Program.cs
@@ -0,0 +1,17 @@
+﻿double AvgMas(int[] a)
+{
+    double s = 0;
+    for (int i = 0; i < a.Length; i++)
+    {
+        s += a[i];
+    }
+    return s / a.Length;
+}
+
+int[] mas = {1,3,5,7,9,11,13,15 };
+for (int i = 0; i < mas.Length; i++)
+{
+    Console.Write(mas[i] + " ");
+}
+Console.WriteLine();
+Console.WriteLine(AvgMas(mas));
