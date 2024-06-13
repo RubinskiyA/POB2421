41b047793c1e6ef6c8264719c5ed7eb748e6e38d
Rubinskii Alex <sasha-mayson@hotmail.com>
2024-06-13 23:52:23 +0200

    hw

---
+++ Lesson21_2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите путь к файлу: ");
+string path = Console.ReadLine();
+FileInfo fileInfo = new FileInfo(path);
+if (fileInfo.Exists) 
+{ 
+    Console.WriteLine("Размер в байтах "+fileInfo.Length);
+    Console.WriteLine("Дата изменения "+fileInfo.LastWriteTime);
+    Console.WriteLine("Расширение "+fileInfo.Extension);
+}
+else
+{
+    Console.WriteLine("Файл не найден.");
+}
