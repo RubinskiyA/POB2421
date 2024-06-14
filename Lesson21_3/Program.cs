0df5c49bab1420d362a14d358dd726ece5cc221c
Rubinskii Alex <sasha-mayson@hotmail.com>
2024-06-14 10:27:39 +0200

    hw

---
+++ Lesson21_3/Program.cs
@@ -0,0 +1,21 @@
+﻿string current = "";
+    current=Directory.GetCurrentDirectory();
+do
+{
+    Console.WriteLine("Текущая директория: "+current);
+    Console.Write("Введите название папки для перехода(или /выход/ для завершения): ");
+    string newPath = Console.ReadLine();
+    if (newPath != "выход")
+    {
+        DirectoryInfo newDir = new DirectoryInfo(current + "/" + newPath);
+        if ((!newDir.Exists))
+        {
+            newDir.Create();
+            current = current + "/" + newPath;
+        }
+        else current = current + "/" + newPath;
+    }
+    else Environment.Exit(0);
+
+}
+while (true);
