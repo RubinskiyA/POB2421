584c5bffcdb6e1d42ca499df59caf382748adcba
Rubinskii Alex <sasha-mayson@hotmail.com>
2024-06-16 22:33:06 +0200

    hw

---
+++ Lesson21_4/Program.cs
@@ -0,0 +1,9 @@
+﻿do
+{
+    Console.Write("Введите путь к файлу или папке: ");
+    string path = Console.ReadLine();
+    if (File.Exists(path)) { File.Delete(path); Console.WriteLine("Файл удален.");}
+    else if (Directory.Exists(path)) { Directory.Delete(path); Console.WriteLine("Папка удалена."); }
+    else Console.WriteLine("Файла или директории не существует.");
+}
+while (true);
