850f291f4186e3f4ff124f2acc4916faa28e13b8
Rubinskii Alex <sasha-mayson@hotmail.com>
2024-06-16 22:48:09 +0200

    hw

---
+++ Lesson22_2/Program.cs
@@ -0,0 +1,19 @@
+﻿do
+{
+    try
+    {
+        Console.Write("Введите числа разделенные пробелом: ");
+        string numbers = Console.ReadLine();
+        string[] num = numbers.Split(" ");
+        int sumNum = 0;
+        foreach (string number in num)
+        {
+            sumNum += int.Parse(number);
+        }
+        Console.WriteLine("Сумма введенных чисел: " + sumNum);
+    }
+    catch (FormatException e)
+    {
+        Console.WriteLine(e.Message);
+    }
+} while (true);
