3f40335453c73cffc45448f7b83a24c32db2e5da
SuperPC <SuperPC@SUPERPC>
2024-07-01 11:28:21 +0200

    hw

---
+++ Lesson24_2/Program.cs
@@ -0,0 +1,19 @@
+﻿try
+{
+    Console.WriteLine("Введите число: ");
+    int num;
+    int.TryParse(Console.ReadLine(), out num);
+    void Numbers(int num, int startNum=1)
+    {
+        
+        Console.Write(startNum+" ");
+        startNum += 2;
+        if (startNum >= num) return;
+        else Numbers(num, startNum);
+    }
+    Numbers(num);
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
