0ff06ffa70a691347f8afbf64cf1dc9c829bb818
SuperPC <SuperPC@SUPERPC>
2024-07-16 16:28:20 +0200

    hw

---
+++ Lesson27_3/Numbers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson27_3
+{
+    internal class Numbers
+    {
+        public int CalculateAverage(int[] mas)
+        {
+            int avgMas = 0;
+            for (int i = 0; i < mas.Length; i++)
+            {
+                avgMas += mas[i];
+            }
+            return avgMas/mas.Length;
+        }
+        public bool IsPrime(int n)
+        {
+            int count = 0;
+            for (int i = 1; i < n; i++)
+            {
+                if (n % i == 0) count++;
+            }
+            if (count<2) return true;
+            else return false;
+        }
+        public bool IsEven(int n)
+        {
+            if (n%2 == 0) return true;
+            else return false;
+        }
+        public bool IsOdd(int n)
+        {
+            if (n % 2 == 0) return false;
+            else return true;
+        }
+
+    }
+}
---
+++ Lesson27_3/Program.cs
@@ -0,0 +1,25 @@
+﻿using Lesson27_3;
+using System.Globalization;
+
+Numbers numbers = new Numbers();
+//Среднее значение массива:
+//int[] mas = { 1, 2, 3, 4, 5};
+//Console.WriteLine(numbers.CalculateAverage(mas));
+////---------------------------------
+//Проверка числа на простое/нет
+//Console.WriteLine("Введите число: ");
+//int num1;
+//int.TryParse(Console.ReadLine(), out num1);
+//Console.WriteLine(numbers.IsPrime(num1));
+//---------------------------------
+//Проверка числа на четность
+//Console.WriteLine("Введите число: ");
+//int num2;
+//int.TryParse(Console.ReadLine(), out num2);
+//Console.WriteLine(numbers.IsEven(num2));
+//------------------------------------
+//Проверка числа на нечетность
+Console.WriteLine("Введите число: ");
+int num2;
+int.TryParse(Console.ReadLine(), out num2);
+Console.WriteLine(numbers.IsOdd(num2));
