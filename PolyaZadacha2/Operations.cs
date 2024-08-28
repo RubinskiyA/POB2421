2af05b743d26fe5c462e96b9ee85b383f1b76f2e
SuperPC <SuperPC@SUPERPC>
2024-08-28 16:52:34 +0200

    hw

--- PolyaZadacha2/Operations.cs
+++ PolyaZadacha2/Operations.cs
@@ -8,16 +8,96 @@
         {
             for (int i = 0; i < 3; i++)
             {
-                Console.WriteLine("Введите " + (i + 1) + " строку: ");
+                Console.Write("Введите " + (i + 1) + " строку: ");
                 ListString.Add(Console.ReadLine()!);
             }
         }
         public void Print()
         {
-            for (int i = 0; i < 3; i++)
+            if (ListString.Count == 0)
             {
-                ListString.ForEach(Console.WriteLine);
+                Console.WriteLine("Список пуст");
+            }
+            else
+            {
+                Console.WriteLine("Список доступных строк: ");
+                for (int i = 0; i < ListString.Count; i++)
+                {
+                    Console.WriteLine(i + " " + ListString[i]);
+                }
             }
         }
+        public void Delete()
+        {
+            if (ListString.Count != 0)
+            {
+                try
+                {
+                    Console.WriteLine("Введите индекс для удаления строки: ");
+                    int ind;
+                    int.TryParse(Console.ReadLine(), out ind);
+                    ListString.RemoveAt(ind);
+                    Print();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+        public void Add()
+        {
+            Console.WriteLine("Введите содержимое новой строки: ");
+            string newString = Console.ReadLine()!;
+            ListString.Add(newString);
+            Print();
+        }
+        public void Clear()
+        {
+            Console.WriteLine("Вы уверены, что хотите очистить список?(да/нет): ");
+            string answer = Console.ReadLine()!;
+            if (answer == "да"||answer=="yes") ListString.Clear();
+            Print();
+        }
+        public void Find()
+        {
+            Console.WriteLine("Введите значение для поиска: ");
+            string answer = Console.ReadLine()!;
+            if (ListString.IndexOf(answer) != -1) Console.WriteLine("Элемент найдет с индексом " + ListString.IndexOf(answer));
+            else Console.WriteLine("Элемент не найден");
+        }
+        public void Sort()
+        {
+            ListString.Sort();
+            Print();
+        }
+        public void FindAllValues()
+        {
+            Console.WriteLine("Введите значение для поиска: ");
+            string value= Console.ReadLine()!;
+            int count = 0;
+            int[] mas= new int[count];
+            for (int i = 0;i<ListString.Count;i++)
+            {
+                if (ListString[i].Equals(value))
+                {
+                    count++;
+                    Array.Resize(ref mas, count);
+                    mas[count - 1] = i;
+                }
+            }
+            if (count == 0) Console.WriteLine("Элемент не найден.");
+            else
+            {
+                Console.WriteLine("Элементы найдены под индексами: ");
+                for (int i = 0; i < count; i++)
+                {
+                    Console.Write(mas[i] + " ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+
     }
 }
--- PolyaZadacha2/Program.cs
+++ PolyaZadacha2/Program.cs
@@ -1,3 +1,21 @@
 ﻿using PolyaZadacha2;
 
 Operations operations = new Operations();
+operations.Print();
+do
+{
+    Console.WriteLine("Выберите операцию для строк: \n1 - добавить строку в список. \n2 - удалить строку\n3 - найти строку по значению\n4 - очистка списка строк\n5 - отсортировать список по алфавитному порядку\n6 - искать все индексы вхождения");
+    int oper;
+    int.TryParse(Console.ReadLine(), out oper);
+    switch (oper)
+    {
+        case 1: operations.Add(); break;
+        case 2: operations.Delete(); break;
+        case 3: operations.Find(); break;
+        case 4: operations.Clear(); break;
+        case 5: operations.Sort(); break;
+        case 6: operations.FindAllValues(); break;
+        default: break;
+    }
+}
+while (true);
