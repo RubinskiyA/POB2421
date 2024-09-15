1144ec77de5156af177bea5a2cd163b780037477
Rubinskii Alex <sasha-mayson@hotmail.com>
2024-09-15 12:33:49 +0200

    hw

---
+++ Lesson27_2/BankAccount.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson27_2
+{
+    internal class BankAccount
+    {
+        private decimal balance;
+
+        public BankAccount(decimal balance)
+        {
+            this.balance = balance;
+        }
+        // Метод для пополнения счета
+        public void Deposit(decimal amount)
+        {
+            if (amount > 0)
+            {
+                balance += amount;
+                Console.WriteLine($"Пополнено на {amount:C}. Новый баланс: {balance:C}");
+            }
+            else
+            {
+                Console.WriteLine("Сумма пополнения должна быть положительной.");
+            }
+        }
+
+        // Метод для снятия денег со счета
+        public void Withdraw(decimal amount)
+        {
+            if (amount > 0)
+            {
+                if (amount <= balance)
+                {
+                    balance -= amount;
+                    Console.WriteLine($"Снято {amount:C}. Новый баланс: {balance:C}");
+                }
+                else
+                {
+                    Console.WriteLine("Недостаточно средств на счете.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Сумма снятия должна быть положительной.");
+            }
+        }
+
+        // Метод для проверки баланса
+        public decimal GetBalance()
+        {
+            return balance;
+        }
+
+        // Метод для отображения информации о счете
+        public void DisplayAccountInfo()
+        {
+            Console.WriteLine($"Текущий баланс: {balance:C}");
+        }
+    }
+}
---
+++ Lesson27_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using Lesson27_2;
+
+// Создание нового банковского счета с начальным балансом 1000
+BankAccount account = new BankAccount(1000m);
+
+// Отображение информации о счете
+account.DisplayAccountInfo();
+
+// Пополнение счета
+account.Deposit(250m);
+
+// Снятие денег со счета
+account.Withdraw(100m);
+
+// Попытка снятия денег сверх доступного баланса
+account.Withdraw(2000m);
+
+// Отображение информации о счете после операций
+account.DisplayAccountInfo();
