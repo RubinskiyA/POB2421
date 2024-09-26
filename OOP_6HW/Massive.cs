e10352d322a29c3680a1c7d0ece9d7ff34bfaebd
SuperPC <SuperPC@SUPERPC>
2024-09-26 10:23:08 +0200

    hw

---
+++ OOP_6HW/Massive.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_6HW
+{
+    internal class Massive
+    {
+        public static double CalculateAverage(int[] numbers)
+        {
+            
+            if (numbers.Length == 0)
+            {
+                throw new ArgumentException("Массив не должен быть пустым");
+            }
+
+            
+            int sum = 0;
+            foreach (int num in numbers)
+            {
+                sum += num;
+            }
+
+            
+            return (double)sum / numbers.Length;
+        }
+    }
+}
---
+++ OOP_6HW/Program.cs
@@ -0,0 +1,7 @@
+﻿using OOP_6HW;
+
+int[] numbers = { 5, 10, 15, 20, 25 };
+
+
+double average = Massive.CalculateAverage(numbers);
+Console.WriteLine($"Среднее значение: {average}");
