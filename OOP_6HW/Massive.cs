4c5f499b07ff9dd130f3151aa01314a8a0bfd30a
SuperPC <SuperPC@SUPERPC>
2024-09-26 10:29:26 +0200

    hw

--- OOP_6HW/Massive.cs
+++ OOP_6HW/Massive.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace OOP_6HW
 {
@@ -25,6 +26,21 @@ namespace OOP_6HW
 
             
             return (double)sum / numbers.Length;
+        }
+        public static void ReverseMas(char[] mas)
+        {
+            if (mas.Length <= 1)
+            {
+                return; 
+            }
+
+            
+            for (int i = 0; i < mas.Length / 2; i++)
+            {
+                char temp = mas[i];
+                mas[i] = mas[mas.Length - i - 1];
+                mas[mas.Length - i - 1] = temp;
+            }
         }
     }
 }
--- OOP_6HW/Program.cs
+++ OOP_6HW/Program.cs
@@ -5,3 +5,9 @@ int[] numbers = { 5, 10, 15, 20, 25 };
 
 double average = Massive.CalculateAverage(numbers);
 Console.WriteLine($"Среднее значение: {average}");
+
+char[] chars = { 'a', 'b', 'c', 'd', 'e', 'f' };
+
+Console.WriteLine("Исходный массив: " + new string(chars));
+Massive.ReverseMas(chars);
+Console.WriteLine("Измененный массив: " + new string(chars));
